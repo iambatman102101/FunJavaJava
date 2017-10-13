791694e95301778154857f24b9279b978d9b3423
iambatman102101 <camrynnjwaite01@gmail.com>
2017-10-13 09:54:50 -0600

    A new change

--- 10GitTest/src/GitTest.java
+++ 10GitTest/src/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("A Git Test!");
 		System.out.println("A line to demonstrate a change.");
+		System.out.println("Another change to the Git test.");
 
 	}
 
